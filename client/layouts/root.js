240dced363aa00cd834423cf5af5b5641c6da56e
Alfonso Duarte <alfonsoduarte@gmail.com>
2016-08-03 23:28:14 -0600

    AVISOS 2

--- client/layouts/root.js
+++ client/layouts/root.js
@@ -7,34 +7,46 @@ angular.module("casserole")
  	
  	if(Meteor.user() && Meteor.user().roles && Meteor.user().roles[0] != "admin"){
 
-	 	this.subscribe('campus', function(){
-			return [{
-				_id : Meteor.user() != undefined ? Meteor.user().profile.campus_id : ""
-			}]
-		});
-		
-		this.subscribe('secciones', function(){
-			return [{
-				_id : Meteor.user() != undefined ? Meteor.user().profile.seccion_id : ""
-			}]
-		});
-	 	
-	 	this.autorun(function() {
+		 	this.subscribe('campus', function(){
+				return [{
+					_id : Meteor.user() != undefined ? Meteor.user().profile.campus_id : ""
+				}]
+			});
+			
+			this.subscribe('secciones', function(){
+				return [{
+					_id : Meteor.user() != undefined ? Meteor.user().profile.seccion_id : ""
+				}]
+			});
 		 	
-	    if(Meteor.user() && Meteor.user()._id){
-	      rc.usuarioActual=Meteor.user();
-	    }
-	    
-	  });
-	  
-	  this.helpers({
-		  campus : () => {
-			  return Campus.findOne(Meteor.user().profile.campus_id);
-		  },
-		  seccion : () => {
-			  return Secciones.findOne(Meteor.user().profile.seccion_id);
-		  }
-	  });
+		 	this.autorun(function() {
+			 	
+		    if(Meteor.user() && Meteor.user()._id){
+		      rc.usuarioActual=Meteor.user();
+		    }
+		    
+		  });
+		  
+		  this.helpers({
+			  campus : () => {
+				  return Campus.findOne(Meteor.user().profile.campus_id);
+			  },
+			  seccion : () => {
+				  return Secciones.findOne(Meteor.user().profile.seccion_id);
+			  }
+		  });
+ 	}
+ 	
+ 	if(Meteor.user() && Meteor.user().roles && Meteor.user().roles[0] == "director"){
+ 			this.subscribe('avisos',()=>{
+				return [{}]
+			});
+			
+			this.helpers({
+				avisos : () => {
+				  return Avisos.find();
+			  }
+		  });		
  	}
  	
  	 this.muestraAvisos = function(){
